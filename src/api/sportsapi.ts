afd36c5827a5e4f334efa6421295d583ef97e567
aceberry924 <meobius123@gmail.com>
2020-09-29 21:26:55 +0800

    add axios and load api from opentdb

--- src/api/sportsapi.ts
+++ src/api/sportsapi.ts
@@ -1,7 +1,17 @@
-
+import Axios from "axios";
+import { QuestionResponse } from "../types/Question";
 
 // TODO implement: https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple
 
-export const getQuestions = () => { 
-    return JSON.parse(`{"response_code":0,"results":[{"category":"Sports","type":"multiple","difficulty":"medium","question":"Which of these teams is a member of the NHL era?","correct_answer":"Philadelphia Flyers","incorrect_answers":["New York Rangers","Toronto Maple Leafs","Boston Bruins"]}]}`);
- }
+export const getQuestions = () => {
+  const url =
+    "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
+
+  return Axios.get(url)
+    .then((response) => response.data)
+    .then((data: QuestionResponse) => {
+      console.log(data);
+      return data;
+    })
+    .catch((err) => console.log(`fetching the quiz failed ${err}`));
+};
